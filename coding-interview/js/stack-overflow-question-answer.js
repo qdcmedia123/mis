454c742fbce5eb5750aadada7fa76a765a8e49c6
Bharat <bharat@wealthface.com>
2020-09-07 13:37:55 +0400

    some more codes is added

--- coding-interview/js/stack-overflow-question-answer.js
+++ coding-interview/js/stack-overflow-question-answer.js
@@ -1,3 +1,20 @@
+// Given an array find a pair such that it sums to a given number
+let nums = [2, 7, 10, 1, 11, 15, 9]
+let target = 11
+let numsMap = new Map()
+let pairs = nums.reduce((acc, num) => {
+  let numToFind = target - num
+  if (numsMap.get(numToFind)) {
+    return [...acc, [num, numToFind]]
+  } else {
+    numsMap.set(num, true)
+    return [...acc]
+  }
+}, [])
+
+console.log("Pairs ", pairs)
+
+
 //JavaScript equivalent of Python's format() function?
 
 String.prototype.format = function () {
